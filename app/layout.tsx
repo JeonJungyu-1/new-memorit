5c2a49faec1895a91a63f5a7fe2b5983a67b65db
JeonJungyu-1 <wjswnsrb93@naver.com>
2025-01-16 21:02:15 +0900

    build: 프로젝트 의존성 패키지 업데이트
    - package.json 및 pnpm-lock.yaml 의존성 버전 업데이트
    - app/layout.tsx 레이아웃 구조 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,9 @@ import { Inter } from 'next/font/google'
 export const metadata = {
   metadataBase: new URL('https://postgres-prisma.vercel.app'),
   title: 'Vercel Postgres Demo with Prisma',
+
   description:
-    'A simple Next.js app with Vercel Postgres as the database and Prisma as the ORM',
+    'A simple Next.js app with Vercel Postgres as the database and Prisma as the ORM'
 }
 
 const inter = Inter({
